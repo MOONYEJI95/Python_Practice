02d7ed0c88279dcb9620472ee08bce5fb51f1c2a
MOONYEJI95 <137989134+MOONYEJI95@users.noreply.github.com>
2023-08-02 21:34:56 +0900

    0802

--- 프로그래머스0731.py
+++ 프로그래머스0731.py
@@ -2,16 +2,19 @@ def solution(code) :
     mode = 0
     ret=''
     for i in range(len(code)) :
-        if mode == 0 and code[i] != 1 and i%2==0 :
+        if mode == 0 and code[i] !='1' and i%2==0 :
             ret = ret + code[i]
-        elif mode == 0 and code[i] ==1 :
+        elif mode == 0 and code[i] == '1' :
             mode = 1
-        elif mode == 1 and code[i] !=1 and i%2==1 :
+        elif mode == 1 and code[i] !='1' and i%2==1 :
             ret = ret + code[i]
-        elif mode == 1 and code[i] ==1 :
+        elif mode == 1 and code[i] =='1' :
             mode = 0
         else : 
             pass
+
+    if ret == ''  :
+        ret = "EMPTY"
     print(ret)    
     
 solution("abc1abc1abc")
